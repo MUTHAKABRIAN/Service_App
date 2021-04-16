fd3f4ac78f70a2823264bec662b9d2f50fcfd9b8
ShemBrooklyne <shemnyatti@gmail.com>
2021-04-16 14:39:25 +0300

    Home interface

--- app/src/main/java/com/moringaschool/serviceapp/MainActivity.java
+++ app/src/main/java/com/moringaschool/serviceapp/MainActivity.java
@@ -1,10 +1,25 @@
 package com.moringaschool.serviceapp;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setDisplayShowCustomEnabled(true);
+        getSupportActionBar().setCustomView(R.layout.search_view);
+//        View view = getSupportActionBar().getCustomView();
+
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
